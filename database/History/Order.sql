68fc11a740d7669d1595bab45eb419df7cad919b
Davide De Pretto <davide.depretto@icloud.com>
2021-06-19 16:51:21 +0200

    Reflect changes of current tables

--- database/History/Order.sql
+++ database/History/Order.sql
@@ -1,12 +1,11 @@
 create table [History].[Order] (
-    [OrderId]           uniqueidentifier    not null,
-    [EmployeeId]        uniqueidentifier    null,
-    [CustomerId]        uniqueidentifier    not null,
-    [OrderDate]         datetime2(0)        not null,
+    [OrderId]               uniqueidentifier    not null,
+    [EmployeeId]            uniqueidentifier    null,
+    [CustomerId]            uniqueidentifier    null,
+    [OrderDate]             datetime2(0)        not null,
 
-    [_active]           bit                 not null,
-    [_validFrom]        datetime2(7)        not null,
-    [_validTo]          datetime2(7)        not null
+    [_validFrom]            datetime2(7)        not null,
+    [_validTo]              datetime2(7)        not null
 ) with (
     data_compression = page
 );
--- database/History/OrderDetail.sql
+++ database/History/OrderDetail.sql
@@ -1,13 +1,13 @@
 create table [History].[OrderDetail] (
-    [OrderDetailId]     uniqueidentifier    not null,
-    [ProductId]         uniqueidentifier    not null,
-    [Price]             money               not null,
-    [VAT]               numeric(7, 4)       not null,
-    [Quantity]          integer             not null,
+    [OrderDetailId]         uniqueidentifier    not null,
+    [OrderId]               uniqueidentifier    not null,
+    [ProductId]             uniqueidentifier    not null,
+    [ProductPriceWhenAdded] money               not null,
+    [ProductVATWhenAdded]   numeric(7, 4)       not null,
+    [Quantity]              integer             not null,
 
-    [_active]           bit                 not null,
-    [_validFrom]        datetime2(7)        not null,
-    [_validTo]          datetime2(7)        not null
+    [_validFrom]            datetime2(7)        not null,
+    [_validTo]              datetime2(7)        not null
 ) with (
     data_compression = page
 );
