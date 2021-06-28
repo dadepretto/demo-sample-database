ec8adbec0ea33d2e2d2d8e86911c2a5c9f07b0f8
Davide De Pretto <davide.depretto@icloud.com>
2021-06-28 21:13:42 +0200

    Add InboundCRM Customer

---
+++ database/InboundCRM/Tables/Customer.sql
@@ -0,0 +1,14 @@
+create table [InboundCRM].[Customer] (
+    [FirstName]                 nvarchar(64)        not null,
+    [MiddleName]                nvarchar(64)        null,
+    [LastName]                  nvarchar(64)        null,
+    [Email]                     nvarchar(512)       null,
+    [Phone]                     nvarchar(32)        null,
+
+    [_lastUpdate]               datetime2(0)        not null
+        constraint [DF_Customer__lastUpdate] default (sysutcdatetime()),
+
+    constraint [PK_Customer]
+        primary key clustered ([Email])
+) with (data_compression = page);
+go
