8af535e9a55b62c0342ad216a005c2bcfd2a460d
Davide De Pretto <davide.depretto@icloud.com>
2021-06-28 22:15:29 +0200

    Add MiddleNames

--- database/Outbound/Tables/Order.sql
+++ database/Outbound/Tables/Order.sql
@@ -3,9 +3,11 @@ create table [Outbound].[Order] (
         constraint [DF_Order_OrderId] default (newid()),
     [EmployeeId]                uniqueidentifier    null,
     [EmployeeFirstName]         nvarchar(64)        null,
+    [EmployeeMiddleName]        nvarchar(64)        null,
     [EmployeeLastName]          nvarchar(64)        null,
     [CustomerId]                uniqueidentifier    null,
     [CustomerFirstName]         nvarchar(64)        null,
+    [CustomerMiddleName]        nvarchar(64)        null,
     [CustomerLastName]          nvarchar(64)        null,
     [CustomerEmail]             nvarchar(512)       null,
     [OrderDate]                 datetime2(0)        not null
